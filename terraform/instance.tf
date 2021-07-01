f7939339746990f098fbd97ccc30aed860954cee
Marcio de Jesus <marciodejesusrj@gmail.com>
2021-07-01 01:13:40 +0000

    Criar politica para a subscrição entre SNS e SQS, agora sim a mensagem chega no SQS

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -9,13 +9,47 @@ resource "aws_sns_topic_subscription" "sns_1_email_target" {
   endpoint_auto_confirms = true
 }
 
+resource "aws_sqs_queue" "sns_sqs_dlq" {
+  name              = "dlq-1-queue"
+}
+
 resource "aws_sqs_queue" "sqs_1" {
   name = "sqs-1-queue"
+  visibility_timeout_seconds = 300
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.sns_sqs_dlq.arn
+    maxReceiveCount     = 2
+  })
 }
 
 resource "aws_sns_topic_subscription" "sqs_1_sqs_target" {
   topic_arn = aws_sns_topic.sns_1.arn
   protocol  = "sqs"
   endpoint  = aws_sqs_queue.sqs_1.arn
-  endpoint_auto_confirms = true
+  raw_message_delivery = true
+}
+
+resource "aws_sqs_queue_policy" "sqs_1_policy" {
+    queue_url = "${aws_sqs_queue.sqs_1.id}"
+
+    policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.sqs_1.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_sns_topic.sns_1.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
 }
